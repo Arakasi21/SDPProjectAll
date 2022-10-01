413be0daaaad7a15876da4d528eb88b9195f6682
Arakasi21 <40834324+Arakasi21@users.noreply.github.com>
2022-10-01 23:26:46 +0600

    Delete SDP/SDPObserverOne directory

--- SDP/SDPObserverOne/src/com/company/IObservable.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-
-public interface IObservable {
-    public void registerObserver(Observer o);
-    public void removeObserver(Observer o);
-    public void notifyObservers();
-}
